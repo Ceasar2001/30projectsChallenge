2dc91278c5b179c5907b09a86e1a9667e6aa236d
Ceasar2001 <ecleoceasar1@gmail.com>
2025-05-07 20:45:27 +0800

    finishing note app

--- notesapp/script.js
+++ notesapp/script.js
@@ -2,6 +2,12 @@ const notesContainer = document.querySelector(".notes-container");
 const createBtn = document.querySelector(".btn");
 let notes = document.querySelectorAll(".input-box");
 
+function showNotes(){
+    notesContainer.innerHTML = localStorage.getItem("notes");
+}
+
+showNotes();
+
 function updateStorage(){
     localStorage.setItem("notes", notesContainer.innerHTML);
 }
@@ -19,12 +25,19 @@ notesContainer.addEventListener("click", function(e){
     if(e.target.tagName === "IMG"){
         e.target.parentElement.remove();
         updateStorage();
-    }else if(e.target.tagName === "p"){
+    }else if(e.target.tagName === "P"){
         notes = document.querySelectorAll(".input-box");
         notes.forEach(nt => {
-            nt.addEventListener("input", function(){
+            nt.onkeyup = function(){
                 updateStorage();
-            })
+            }
         })
+    }
+})
+
+document.addEventListener("keydown", function(e) {
+    if (e.key === "Enter") {
+        document.execCommand("insertLineBreak");
+        event.preventDefault();
     }
 })
